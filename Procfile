9e6a92d28c15ab235f23aecc4ccbd072171503d4
jnm <jmilner@cristoreyny.org>
2014-01-22 06:16:33 +0000

    Attempt at sftp storage

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 # probably not the best invocation
-# celery usage is archaic
+# celery usage is archaic but somehow works (for the gradebook), even without amqp
 web: gunicorn ecwsp.wsgi & python manage.py celerybeat & python manage.py celeryd
--- django_sis/settings_local.py
+++ django_sis/settings_local.py
@@ -6,7 +6,17 @@ INSTALLED_APPS = (
     'ecwsp.benchmarks',
     'ecwsp.benchmark_grade',
     'ecwsp.work_study',
+    'storages',
 )
+
+DEFAULT_FILE_STORAGE = 'storages.backends.sftpstorage.SFTPStorage'
+SFTP_STORAGE_HOST = 'daphne.cristoreyny.org'
+SFTP_STORAGE_ROOT = '/var/www/heroku_storage/'
+SFTP_STORAGE_PARAMS = {
+    'username': 'OMG NO WAY!',
+    'password': 'SERIOUSLY!',
+}
+MEDIA_URL = 'http://daphne.cristoreyny.org:12345/heroku_storage/media/'
 
 import dj_database_url
 DATABASES = {}
